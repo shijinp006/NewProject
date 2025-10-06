74ac1180fb9ac27be32b598ed1816ba384b97797
shijinp006 <shijinp9404@gmail.com>
2025-10-06 14:11:38 +0530

    first commit

--- vite-project/src/globalSpinner/globalSpinner.tsx
+++ vite-project/src/globalSpinner/globalSpinner.tsx
@@ -9,7 +9,7 @@ export const GlobalSpinner = () => {
 
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-white z-50 p-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full max-w-6xl">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full max-w-6xl">
         {[1, 2, 3, 4].map((i) => (
           <div
             key={i}
